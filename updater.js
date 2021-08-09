1092ff5d902bcc19d457210e7b4502a436c46d21
angyongen <angyongen@gmail.com>
2021-08-10 00:41:18 +0800

    handle multiple directions

--- updater.js
+++ updater.js
@@ -1,7 +1,32 @@
+
+function recorder1() {
+	this.data = {};
+	this.put = function (origin, target, data) {
+		if (!this.data[origin]) this.data[origin] = {};
+		this.data[origin][target] = data;
+	}
+}
+if (typeof wrapper === 'undefined') wrapper = new recorder1();
+
+var arrowLookup = {
+	"NS1":"a2", "NS28":"a1",
+	"EW33": "a2", "EW1": "a1",
+	"TE1": "a1", "TE3": "a2", 
+	"CC1": "a1", "CC29": "a2", 
+	"NE1": "a1", "NE17": "a2", 
+	"DT1": "a2", "DT35": "a1", 
+	"CG2": "a2", "CG": "a1", 
+}
 window.onload = function () {
 	startUpdate(function (data) {
 		for (var key in data) {
-			var element = document.querySelector(".text." + key);
+			var target = key.split(">")[1];
+			var arrow = arrowLookup[target];
+			var arrowKey = key.replace(">"+target, "."+arrow)
+			var origin = key.replace(">"+target, "")
+			if (wrapper) {wrapper.put(origin, target, data[key])}
+
+			var element = document.querySelector(".text." + arrowKey);
 			if (element) {
 				element.textContent = Math.round(data[key][0]) + " min"
 			}
